e7b4618f1ed0965f8e20379996087310e1838927
lekhang-1510 <tieukhang2023@gmail.com>
2026-04-14 19:45:49 +0700

    Update day3_day4_daytext

---
+++ day3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Hello World 2222");
+    return 0;
+}
+// Created by ACER on 12/04/2026.
+//
---
+++ day4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    //Khai báo
+    int a;
+    int sum = 0;
+
+    //Nhập dữ liêu
+    printf("Tinh tong tu 1 --> a ");
+    printf("\nNhap vao a: ");
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++) {
+        printf("\n%d + %d = %d", sum, i, sum + i);
+        sum += i;
+    }
+    //In dữ liêu
+    printf("\n------------------");
+    printf("\nTong tu 1 --> %d = %d ", a, sum);
+    return 0;
+}
