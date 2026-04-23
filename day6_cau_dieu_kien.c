e3c03fd58eacb30ac9372d6117bf3ed402e4ba5e
lekhang-1510 <tieukhang2023@gmail.com>
2026-04-23 20:47:59 +0700

    Update first

---
+++ day6_cau_dieu_kien.c
@@ -0,0 +1,25 @@
+/*Viết chương trình nhập vào một số nguyên
+ * Kiểm tra xem số người dùng nhập vào là số chẵn hay số lẻ
+ * Nếu là số 0 thì in ra không chẵn không lẽ
+ */
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter your number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("The number %d is an even number.", number);
+    } else {
+        printf("The number %d is an odd number.", number);
+    }
+    if (number == 0) {
+        printf("The number %d is neither an even nor an odd number.", number);
+    }
+    return 0;
+}
+
+//
+// Created by ACER on 23/04/2026.
+//
