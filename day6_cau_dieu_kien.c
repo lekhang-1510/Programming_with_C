b176bd93c770e95cafbfc4d89fddd9908e42f931
lekhang-1510 <tieukhang2023@gmail.com>
2026-04-23 21:17:02 +0700

    Update first v3

--- day6_cau_dieu_kien.c
+++ day6_cau_dieu_kien.c
@@ -2,6 +2,9 @@
  * Kiểm tra xem số người dùng nhập vào là số chẵn hay số lẻ
  * Nếu là số 0 thì in ra không chẵn không lẽ
  */
+
+/*
+ *CÁCH 1:
 #include <stdio.h>
 
 int main() {
@@ -16,6 +19,25 @@ int main() {
         } else {
             printf("The number %d is an odd number.", number);
         }
+    }
+    return 0;
+}*/
+
+//CÁCH 2: Dùng if - else if - else
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter your number: ");
+    scanf("%d", number);
+
+    if (number == 0) {
+        printf("The number %d is neither an even nor an odd number.", number);
+    } else if (number % 2 == 0) {
+        printf("The number %d is an even number.", number);
+    } else {
+        printf("The number %d is an odd number.", number);
     }
     return 0;
 }
